2af22fd7b84c8319f51d96d4402ed2cfbb302699
Tanmay <tanmaysayare12345@gmail.com>
2024-01-06 10:33:42 +0530

    update hello world program for readability and maintainability

--- practicals/helloworld.php
+++ practicals/helloworld.php
@@ -1,3 +1,16 @@
 <?php
-    echo "Hello, World!";
+
+/**
+ * Display a greeting message.
+ *
+ * This function prints a simple greeting message to the console.
+ */
+function displayGreeting() {
+    echo "Hello, World!\n";
+}
+
+// Call the function to display the greeting
+displayGreeting();
+
 ?>
+
