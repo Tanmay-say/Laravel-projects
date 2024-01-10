e3d3db144cc7950394f196124c6e165e8f44de67
Tanmay <tanmaysayare12345@gmail.com>
2024-01-10 12:48:37 +0530

    Create Song database model

---
+++ app/Models/Song.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Song extends Model
+{
+    use HasFactory;
+
+    // Add fillable field corresponding to database columns .
+    protected $fillable=['title', 'artist'];
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getArtist() {
+        return $this->artist;
+    }
+}
---
+++ database/seeders/SongSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Song;
+
+class SongSeeder extends Seeder
+{
+    
+
+    public function run(): void
+    {
+
+        // Seed a few songs
+        Song::create([
+            'artist' => 'Eminem',
+            'title' => 'Stan',
+        ]);
+
+        Song::create([
+            'artist' => 'A P Dhillon',
+            'title' => 'With You',
+        ]);
+
+        Song::create([
+            'artist' => 'J D Daku',
+            'title' => 'May I got',
+        ]);
+
+        Song::create([
+            'artist' => 'A P Hello',
+            'title' => 'Jab we met ',
+        ]);
+        
+
+    }
+}
