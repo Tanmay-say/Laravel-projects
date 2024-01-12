2209f767bd6ad5ff92d6583b9ba52fd7235a93ad
Tanmay <tanmaysayare12345@gmail.com>
2024-01-12 12:48:36 +0530

    Create Song database model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Practicals\Song;
+use App\Models\Song;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,27 +31,5 @@ Route::get('//veggies/baigan', function () {
 });
 
 Route::get('/songs', function () {
-    return view('songs');
+    return view('songs', [ 'songs' => Song::all() ] );
 });
-
-Route::get('/songs', function () {
-    $song = new Song();
-    $song->setTitle('With You');
-    return view('songs', [ 'song' => $song ]);
-});
-
-Route::get('/songs', function () {
-    $song1 = new Song();
-    $song1->setTitle("Stan");
-    $song1->setArtist("Eminem");
-  
-    $song2 = new Song();
-    $song2->setTitle("Nothing Else Matters");
-    $song2->setArtist("Metallica");
-  
-    $song3 = new Song();
-    $song3->setTitle("With You");
-    $song3->setArtist("A P Dhillon");
-  
-    return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
-  });
